7a71f3198b0b969e1ca26b3d276865422ddac07c
andypyle <andy.pyle@gmail.com>
2017-03-25 17:36:56 -0700

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import styles from './App.sass';
+
+const App = () => (
+	<main className={styles.Main}>
+		<h1>Application Container</h1>
+	</main>
+)
+
+export default App;
